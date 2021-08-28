e2eca3d7509833573a8fa921529bcbc9c16ee3a5
khashayar <khashayarmahg@gmail.com>
2021-08-28 16:47:24 +0430

    feat(MainJSFile): add aparat advertise project to myworks object

--- src/components/Main.js
+++ src/components/Main.js
@@ -60,6 +60,7 @@ class Main extends Component {
             aparatInfo99: { link : "https://www.aparat.com/info99" } ,
             aparatEuro2020: { link : "https://www.aparat.com/euro2020" } ,
             aparatEuro2020Match: { link : "https://www.aparat.com/euro2020/match" } ,
+            aparatAdvertise: { link : "https://www.aparat.com/advertise" } ,
             televikaDvd: { link : "https://www.televika.com/dvd", needVpn : true } ,
             televikaWelcome: { link : "https://www.televika.com/welcome", needVpn : true } ,
             sabaideaCharity: { link : "https://www.sabaidea.com/charity" } ,
